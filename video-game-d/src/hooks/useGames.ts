41c47e54202ea1e409fa43d5f083702614aec5a5
Abraham Fatoki <fatokiabraham@gmail.com>
2023-03-16 23:04:51 +0100

    displaying platform icons

--- video-game-d/src/hooks/useGames.ts
+++ video-game-d/src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -19,7 +26,7 @@ const useGames = () => {
 
   useEffect(() => {
     const controller = new AbortController();
-    
+
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
